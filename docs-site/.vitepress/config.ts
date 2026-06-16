565e99b07e06c5c448742fe785ebdfb85b85b3fd
Josh Sandlin <joshua.sandlin@gmail.com>
2026-06-15 22:49:50 -0400

    release: v0.3.0

--- docs-site/.vitepress/config.ts
+++ docs-site/.vitepress/config.ts
@@ -41,7 +41,7 @@ export default withMermaid(defineConfig({
       { text: "Examples", link: "/examples/dogfooding", activeMatch: "/examples/" },
       { text: "Reference", link: "/reference/cli", activeMatch: "/reference/" },
       {
-        text: "v0.2.3",
+        text: "v0.3.0",
         items: [
           {
             text: "Changelog",
